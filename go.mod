3c45fb59c346ea9733be659cd902c20acf3b92b2
Igor Tatarinov <62074491+TatarinovIgor@users.noreply.github.com>
2021-11-23 18:15:30 +0200

    login and registration pages added database connected tests temperately removed

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.16
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/heroku/x v0.0.42
+	github.com/jackc/pgx/v4 v4.14.0 // indirect
+	github.com/lib/pq v1.10.4 // indirect
 )
--- main.go
+++ main.go
@@ -1,38 +1,38 @@
 package main
 
 import (
+	"eduapp/scripts"
 	"fmt"
-	"log"
-	"net/http"
-	"os"
-
 	"github.com/gorilla/mux"
 	_ "github.com/heroku/x/hmetrics/onload"
+	"log"
+	"net/http"
 )
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
-}
 
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
-	fmt.Println("router created")
-	r.HandleFunc("/hello", handler).Methods("GET")
+
 	staticFileDirectory := http.Dir("./templates/")
 	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
 	r.PathPrefix("/assets").Handler(staticFileHandler).Methods("GET")
-	r.HandleFunc("/request", getHandler).Methods("GET")
-	r.HandleFunc("/request", createHandler).Methods("POST")
+
+	// Routers GET
+	r.HandleFunc("/register", scripts.RegisterUserPage).Methods("GET")
+	r.HandleFunc("/login", scripts.LoginUserPage).Methods("GET")
+
+	//Routers POST
+	r.HandleFunc("/register", scripts.RegisterUser).Methods("POST")
+	r.HandleFunc("/login", scripts.LoginUser).Methods("POST")
 	return r
 }
 
 func main() {
-	port := os.Getenv("PORT")
+	port := "8080"
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
-
+	scripts.DbConnector()
 	r := newRouter()
 	fmt.Println("hello i am started")
 	err := http.ListenAndServe(fmt.Sprintf(":%s", port), r)
