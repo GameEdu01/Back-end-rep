7adbe8473893c98adfbdc57d3e4fc1cf48a21f6e
RDMchik <denkickgames@gmail.com>
2021-12-27 19:54:46 +0200

    updated sign up and other

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from cryptography import fernet
+from cryptography import fernet # importing all the external packages
 from fastapi import FastAPI
 from fastapi.param_functions import Query
 from pydantic import BaseModel
@@ -6,12 +6,14 @@ import psycopg as pg
 from cryptography.fernet import Fernet
 import requests
 from requests.exceptions import ReadTimeout
+import random
 
 
-key = b'yURsNoRMdtDMy8QUj-05B64K-5cvNaJ-VNxvQZOu154=' # Will be hidden in the future
+key = b'yURsNoRMdtDMy8QUj-05B64K-5cvNaJ-VNxvQZOu154=' # Key used to encrypt secrets, will be hidden in the .env in future
 fernet = Fernet(key)
 
-class DBConnector: # Connetion to the database
+
+class DBConnector: # Connetion to the database class
 
     def __init__(self, dbname, user, password, address):
 
@@ -24,7 +26,8 @@ class DBConnector: # Connetion to the database
         self.cursor = self.conn.cursor()
 
         self.queries = {"get_table": ["SELECT * FROM ", " ORDER BY id"],
-                        "username_exists": ["SELECT EXISTS(SELECT 1 FROM ", " WHERE username = '", "')"]}
+                        "username_exists": ["SELECT EXISTS(SELECT 1 FROM ", " WHERE username = '", "')"],
+                        "email_exists": ["SELECT EXISTS(SELECT 1 FROM ", " WHERE email = '", "')"]}
         
     def connect(self): # getting the connection
 
@@ -64,8 +67,23 @@ class DBConnector: # Connetion to the database
 
         return userExists
 
+    def email_exists(self, tableName, email): # check if email exist in certain database
 
-class APIConnecter: # Requests to already existing api, but i think everything should've been written in the fastapi
+        emailExists = False
+
+        query = self.queries["email_exists"][0] + tableName + self.queries["email_exists"][1] + email + self.queries["email_exists"][2]
+
+        self.cursor.execute(query)
+
+        emailExists = self.cursor.fetchall()[0][0]
+
+        return emailExists
+
+
+# NOTE i am planning to make a single function instead of user_exists() and email_exists()
+
+
+class APIConnecter: # Requests to already existing api class, but i think everything should've been written in the fastapi
 
     def __init__(self, address):
 
@@ -82,28 +100,28 @@ dbc = DBConnector(dbname="d2d1ljqhqhl34q",
                   address=("ec2-54-161-164-220.compute-1.amazonaws.com", "5432"))
 
 
-class Login(BaseModel):
+class Login(BaseModel): # Login basic model
     username: str
     password: str
 
 
-class SignUp(BaseModel):
+class SignUp(BaseModel): # SignUp basic model
     username: str
     password: str
     email: str
 
 
-@app.get("/fastapi/")
+@app.get("/fastapi/") # Hello world!
 def get_root(): 
     return {"Hello": "World!"}
 
 
-@app.post("/fastapi/table")
+@app.post("/fastapi/table") # get table from the database by it's name
 def get_table(table_name: str):
     return dbc.get_table(table_name)
 
 
-@app.post("/fastapi/login_with_go")
+@app.post("/fastapi/login_with_go") # Login with Igor's go api, not synced with the rest of fastapi i wrote (takes a lot of time to run)
 def login(login: Login):
 
     request = requests.post(apic.address + apic.postLocations["login"],
@@ -114,20 +132,31 @@ def login(login: Login):
 
     return {"message": "There was an error login in, %s".format(request.status_code)}
 
+# I created new "users" database for the signup function, i hope you like it so we can keep it for now
 
-@app.post("/fastapi/signup")
+@app.post("/fastapi/signup") # Signing up, all secrity measures are in there, basic syntax check is also included
 def signup(signup: SignUp):
 
+    if " " in signup.email or not "@" in signup.email:
+        return {"message": "Please use an appropriate email address"}
+    if " " in signup.username:
+        return {"message": "Username can not contain spaces"}
+    if " " in signup.password:
+        return {"message": "Password can not contain spaces"}
+
     userExists = dbc.user_exists("users", signup.username)
-    if userExists:
-        return {"message": "This username already exists!"}
+    emailExists = dbc.user_exists("users", signup.username)
+    if userExists or emailExists:
+        return {"message": "This username or email already exists!"}
 
-    signup.password = fernet.encrypt(signup.password.encode())
-    signup.password = signup.password.decode()
+    signup.password = fernet.encrypt(signup.password.encode()).decode()
 
-    dbc.cursor.execute("""INSERT INTO users (username, password, email)
-                            VALUES ('{}', '{}', '{}')""".format(signup.username, signup.password, signup.email))
+    token = Fernet.generate_key().decode()
+
+    dbc.cursor.execute("""INSERT INTO users (username, password, email, token)
+                            VALUES ('{}', '{}', '{}', '{}')""".format(signup.username, signup.password, signup.email, token))
 
     dbc.conn.commit()
 
-    return {"message": f"{signup.username} was added to database.users"}
+    return {"message": f"{signup.username} was added to database",
+            "token": token}
