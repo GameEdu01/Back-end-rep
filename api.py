f0092ea9cc3bd4d961898bde6f663f2b8bc9f736
RDMchik <denkickgames@gmail.com>
2021-12-28 15:35:21 +0200

    big session commit

--- api.py
+++ api.py
@@ -411,7 +411,7 @@ def signup(signup: SignUp):
 # If you want to run this script localy on your machine:
 # uvicorn api:app --reload      
 
-# Then go into http://127.0.0.1:800/docs and you'll see the documentation
+# Then go into http://127.0.0.1:8000/docs and you'll see the documentation
 
 # If even after all you saw here you still don't want to integrate whole api into fastapi, please make your
 # own documentation (and again, FastAPI makes it automatically), or just explain me a bit more about 
